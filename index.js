21f6267c28f553ca508c9ef0877526a017620003
praisonjoseph <praison.joseph@gmail.com>
2023-03-28 21:19:44 +0530

    Added the views and assets

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 8000;
-const path = require('path')
 
 const db = require('./config/mongoose')
 const ToDo = require('./models/todo')
 app.set('views.engine', 'ejs')
 app.set('views', './views')
-
+app.use(express.static('assets'))
+app.use(express.urlencoded())
 
 app.listen(port, function(err) {
     if (err) {
